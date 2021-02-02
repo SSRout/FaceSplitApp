298c5e3a4894bf6696ac7b9267e629285ac62eff
ssrout <smruti.sambit.rout@gmail.com>
2021-02-02 23:22:14 +0530

    Added messagingQueue

---
+++ MessagingQueue/Commands/IRegisterOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingQueue.Commands
+{
+    public interface IRegisterOrderCommand
+    {
+        public string PictureUrl { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
---
+++ MessagingQueue/Constants/RabbitMqMassTransitConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessagingQueue
+{
+    public class RabbitMqMassTransitConstant
+    {
+        public const string RabbitMqUri = "rabbitmq://rabbitmq:5672";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string RegisterOrderServiceQueue = "register.order.command";
+    }
+}
