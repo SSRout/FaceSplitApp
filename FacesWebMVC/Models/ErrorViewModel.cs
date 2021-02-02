4051312e97a3738bd71971917400cf3c1020fd79
ssrout <smruti.sambit.rout@gmail.com>
2021-02-02 23:24:12 +0530

    added web client

---
+++ FacesWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FacesWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
